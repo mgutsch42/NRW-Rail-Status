6665c80854f5049b6c14a94483658ff76a4ce90d
mgutsch42 <100157940+mgutsch42@users.noreply.github.com>
2026-06-13 22:19:02 +0200

    Update config_flow.py

--- custom_components/nrw_rail_status/config_flow.py
+++ custom_components/nrw_rail_status/config_flow.py
@@ -2,8 +2,8 @@
 
 from __future__ import annotations
 
+import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
@@ -15,7 +15,20 @@ class NRWConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        if user_input is not None:
-            return self.async_create_entry(title="NRW Rail Status", data={})
 
-        return self.async_show_form(step_id="user", data_schema=None)
+        # Nur eine Instanz erlauben
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        # Wenn der User bestätigt → Entry erstellen
+        if user_input is not None:
+            return self.async_create_entry(
+                title="NRW Rail Status",
+                data={}
+            )
+
+        # Leeres Formular anzeigen
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({})
+        )
