248b71e91782afba01775e13418cace58070d8be
mgutsch42 <100157940+mgutsch42@users.noreply.github.com>
2026-06-14 17:37:51 +0200

    Update api.py

--- custom_components/nrw_rail_status/api.py
+++ custom_components/nrw_rail_status/api.py
@@ -288,8 +288,8 @@ class NRWHimApi:
             _LOGGER.error("Antwort war: %s", raw_text)
             return []
 
-        msgL = svc.get("msgL", [])
-        common = svc.get("common", {})
+        msgL = svc.get("msgL", {})
+        common = svc.get("himL", [])
 
         # Schritt 6: NRWMessage-Objekte erzeugen
         messages = []
